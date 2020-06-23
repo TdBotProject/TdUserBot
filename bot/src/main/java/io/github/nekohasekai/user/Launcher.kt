6ed62e5a9cab030b0ac030e8c6558085d0c66e23
世界 <i@nekox.me>
2020-06-23 20:26:25 +0800

    Add binlog import/export support

--- bot/src/main/java/io/github/nekohasekai/user/Launcher.kt
+++ bot/src/main/java/io/github/nekohasekai/user/Launcher.kt
@@ -1,7 +1,10 @@
 package io.github.nekohasekai.user
 
+import cn.hutool.log.level.Level
 import io.github.nekohasekai.nekolib.cli.TdCli
 import io.github.nekohasekai.nekolib.cli.TdLoader
+import io.github.nekohasekai.nekolib.cli.bl.ExportBinlog
+import io.github.nekohasekai.nekolib.core.client.TdClient
 import io.github.nekohasekai.nekolib.core.raw.deleteChatMessagesFromUser
 import io.github.nekohasekai.nekolib.core.raw.getChat
 import io.github.nekohasekai.nekolib.core.raw.getUser
@@ -14,6 +17,8 @@ import td.TdApi
 
 object Launcher : TdCli() {
 
+    lateinit var parameters: Array<String>
+
     override val loginType = LoginType.USER
 
     init {
@@ -25,6 +30,8 @@ object Launcher : TdCli() {
     @JvmStatic
     fun main(args: Array<String>) {
 
+        parameters = args
+
         TdLoader.tryLoad()
 
         start()
@@ -32,6 +39,22 @@ object Launcher : TdCli() {
     }
 
     override fun onLoad() {
+
+        if (parameters.isNotEmpty()) {
+
+            when (parameters.getOrNull(0)) {
+
+                "export" -> {
+
+                    addHandler(ExportBinlog())
+
+                    return
+
+                }
+
+            }
+
+        }
 
         addHandler(CleanDA())
 
